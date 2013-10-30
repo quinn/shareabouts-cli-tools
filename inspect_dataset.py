89b673a7f5651c8c7b5e3f81c1487936a39448f7
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-10-30 14:05:06 -0400

    Add field output to inspect_dataset

--- inspect_dataset.py
+++ inspect_dataset.py
@@ -10,7 +10,25 @@ import json
 def main(config):
     tool = ShareaboutsTool(config['host'])
     all_places = tool.get_places(config['owner'], config['dataset'])
-    tool.get_source_place_map(all_places)
+    all_submissions = tool.get_submissions(config['owner'], config['dataset'])
+
+    place_fields = set()
+    for place in tool.api.account(config['owner']).dataset(config['dataset']).places:
+        place_fields.update(place._data['properties'].keys())
+
+    print('Place fields: ')
+    for field in place_fields:
+        print('  - %s' % (field,))
+
+    for sset in tool.api.account(config['owner']).dataset(config['dataset']).submission_sets:
+        submission_fields = set()
+
+        for submission in sset:
+            submission_fields.update(submission._data.keys())
+
+        print('%s fields:' % (sset.name.title(),))
+        for field in submission_fields:
+            print('  - %s' % (field,))
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Print the number of places in a dataset.')
