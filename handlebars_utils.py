ae06a2358f74b7a2569947d757cf97fcaf6f0779
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-11-14 12:17:11 -0500

    More template helpers - Update group_by helpers to use a key function if provided - Add a sort_by helper for iterable contexts - Add arbitrary-length conjunction (if_all) and disjunction (if_any) helpers

--- handlebars_utils.py
+++ handlebars_utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 from pybars import strlist
 from pybars._compiler import resolve, _if, _each
@@ -50,42 +51,62 @@ def _format(this, value, format):
             pass
     return value
 
-def _group_by(this, options, *args):
+def _sort_by(this, options, *args):
+    # If there's one argument, it's the sorting attribute name, and this will
+    # be used as the context (iterable).
     if len(args) == 1:
         attr_name = args[0]
         iterable = this
+
+    # If there are two arguments, the first is the context, the second if the
+    # sorting argument.
     else:
         iterable, attr_name = args
 
-    grouped_context = {}
+    # Check the options for the function that generates an sort key. The
+    # function should take two parameters (an element and the name of an
+    # attribute) and generate a sort key.
+    sort_key_maker = options.get(
+        'sort_key_maker',
+        lambda elem, attr: resolve(elem, *attr.split('.'))
+    )
 
+    sorted_context = sorted(iterable, key=lambda elem: sort_key_maker(elem, attr_name))
+    return options['fn'](sorted_context)
+
+def _group_by(this, options, *args):
+    # If there's one argument, it's the grouping attribute name, and this will
+    # be used as the context (iterable).
+    if len(args) == 1:
+        attr_name = args[0]
+        iterable = this
+
+    # If there are two arguments, the first is the context, the second if the
+    # grouping argument.
+    else:
+        iterable, attr_name = args
+
+    # Check the options for the function that generates an group key. The
+    # function should take two parameters (an element and the name of an
+    # attribute) and generate a group key.
+    group_key_maker = options.get(
+        'group_key_maker',
+        lambda elem, attr: resolve(elem, *attr.split('.'))
+    )
+
+    grouped_context = defaultdict(list)
     for elem in iterable:
-        value = elem[attr_name]
-        if value not in grouped_context:
-            grouped_context[value] = [elem]
-        else:
-            grouped_context[value].append(elem)
+        group_key = group_key_maker(elem, attr_name)
+        grouped_context[group_key].append(elem)
 
     return _each(this, options, grouped_context)
 
 def _group_by_date(this, options, *args):
-    if len(args) == 1:
-        attr_name = args[0]
-        iterable = this
-    else:
-        iterable, attr_name = args
-
-    grouped_context = {}
-
-    for elem in iterable:
-        # Assume an ISO8601 String
-        value = elem[attr_name][:10]
-        if value not in grouped_context:
-            grouped_context[value] = [elem]
-        else:
-            grouped_context[value].append(elem)
-
-    return _each(this, options, grouped_context)
+    # The group key should only take in to accound the first 10 characters of
+    # the given attribute, as the first 10 characters in an ISO8601 formatted
+    # date/time string represent the date portion.
+    options['group_key_maker'] = lambda elem, attr: resolve(elem, *attr.split('.'))[:10]
+    return _group_by(this, options, *args)
 
 def _if_gte(this, options, val1, val2):
     return _if(this, options, lambda _: val1 >= val2)
@@ -95,6 +116,12 @@ def _if_lte(this, options, val1, val2):
 
 def _if_equal(this, options, val1, val2):
     return _if(this, options, lambda _: val1 == val2)
+
+def _if_any(this, options, *values):
+    return _if(this, options, any(values))
+
+def _if_all(this, options, *values):
+    return _if(this, options, all(values))
 
 def _with(this, options, local_context):
     return options['fn'](local_context)
@@ -118,11 +145,14 @@ helpers = {
     'if_datetime_changed': _if_datetime_changed,
     'quoted': _quoted,
     'format': _format,
+    'sort_by': _sort_by,
     'group_by': _group_by,
     'group_by_date': _group_by_date,
     'if_lte': _if_lte,
     'if_gte': _if_gte,
     'if_equal': _if_equal,
+    'if_any': _if_any,
+    'if_all': _if_all,
     'with': _with,
     'length': _length
 }
