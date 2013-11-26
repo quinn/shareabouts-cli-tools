8962b0ede88d2a1a93d1f791b771e04f3ef0018b
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-11-26 18:35:07 -0500

    Delete places that no longer exist in the data

--- upload.py
+++ upload.py
@@ -5,26 +5,28 @@ from __future__ import print_function, unicode_literals, division
 from shareabouts_tool import ShareaboutsTool
 from argparse import ArgumentParser
 import json
+import sys
 
 spinner_frames = '\|/―'
 step = 0
 
 def place_done_callback(place, place_response):
-    import sys
     global step
-
-    if place_response.status_code not in (200, 201):
-        print('Error saving place %s: %s (%s)' % (place, place_response.status_code, place_response.text))
-        return
-    elif place_response.status_code == 200:
-        print('\r%s - Saved   %s  ' % (step, spinner_frames[step % 4]), end='')
-    else:
-        print('\r%s - Created %s  ' % (step, spinner_frames[step % 4]), end='')
-    sys.stdout.flush()
 
     step += 1
 
-def main(config, silent=True, create=True, update=True):
+    if place_response.status_code == 200:
+        print('\r%s - Saved   %s  ' % (step, spinner_frames[step % 4]), end='')
+    elif place_response.status_code == 201:
+        print('\r%s - Created %s  ' % (step, spinner_frames[step % 4]), end='')
+    elif place_response.status_code == 204:
+        print('\r%s - Deleted %s  ' % (step, spinner_frames[step % 4]), end='')
+    else:
+        print('Error saving place %s: %s (%s)' % (place, place_response.status_code, place_response.text))
+        return
+    sys.stdout.flush()
+
+def main(config, silent=True, create=True, update=True, delete=False):
     tool = ShareaboutsTool(config['host'])
     all_places = tool.get_places(config['owner'], config['dataset'])
     mapped_places = tool.get_source_place_map(all_places, mapped_id_field=config.get('mapped_id_field', '_imported_id'))
@@ -50,16 +52,34 @@ def main(config, silent=True, create=True, update=True):
         config['owner'], config['dataset'], config['key'],
         loaded_places, place_done_callback, silent=silent, create=create, update=update)
 
+    source_id_field = config.get('source_id_field', None)
+    loaded_ids = set([
+        place['properties'].get(source_id_field or 'id')
+        for place in loaded_places])
+    gone_places = [place for (mapped_id, place) in mapped_places.items() if mapped_id not in loaded_ids]
+    gone_place_ids = [str(place.get('id')) for place in gone_places]
+
+    print('\n%s places are no longer present in the imported data: %s.' % (len(gone_places), ', '.join(gone_place_ids)), file=sys.stderr)
+    print('Deleting the places...', file=sys.stderr)
+
+    if delete:
+        global step
+        step = 1
+        tool.delete_places(
+            config['owner'], config['dataset'], config['key'],
+            gone_places, place_done_callback)
+
     print('\nDone!')
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Print the number of places in a dataset.')
     parser.add_argument('configuration', type=str, help='The configuration file name')
     parser.add_argument('--no-create', dest='create', action='store_false', help='Create non-existant places?')
-    parser.add_argument('--no-update', dest='update', action='store_false' ,help='Update pre-existing places?')
+    parser.add_argument('--no-update', dest='update', action='store_false', help='Update pre-existing places?')
+    parser.add_argument('--no-delete', dest='delete', action='store_false', help='Delete no longer existing places?')
     parser.add_argument('--log-activity', dest='silent', action='store_false' ,help='Log save and update activity?')
 
     args = parser.parse_args()
     config = json.load(open(args.configuration))
 
-    main(config, create=args.create, update=args.update, silent=args.silent)
+    main(config, create=args.create, update=args.update, delete=args.delete, silent=args.silent)
